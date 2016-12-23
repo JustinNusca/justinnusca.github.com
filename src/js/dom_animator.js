ede71d5dcdbd2a77a12b0c05cbe2ba09c574471a
Justin <njustin@gmail.com>
2016-12-22 21:00:13 -0800

    Add DOM enter anims & form handlers.

---
+++ src/js/dom_animator.js
@@ -0,0 +1,110 @@
+class DomAnimationElement {
+  constructor(element) {
+    this.animClass = element.getAttribute('data-anim-name');
+    this.direction = element.getAttribute('data-anim-direction');
+    this.element = element;
+    this.originY = element.offsetTop;
+    this.type = element.getAttribute('data-anim-type');
+  }
+
+  addClass() {
+    if (this.hasClass()) { return; }
+    this.element.classList.add(this.animClass);
+  }
+
+  handleClass(triggered) {
+    if (triggered) {
+      this.addClass();
+    } else if (this.type === 'toggle' && this.hasClass()) {
+      this.removeClass();
+    }
+  }
+
+  animate() {
+    if (this.direction === 'in') {
+      this.handleClass(this.scrolledIn());
+    } else {
+      this.handleClass(this.scrolledOut());
+    }
+  }
+
+  removeClass() {
+    this.element.classList.remove(this.animClass);
+  }
+
+  hasClass() {
+    return this.element.classList.contains(this.animClass);
+  }
+
+  resetOffsetY() {
+    this.removeClass();
+    this.originY = this.element.offsetTop;
+    this.animate();
+  }
+
+  scrolledIn() {
+    const viewTop = window.pageYOffset;
+    const viewBottom = viewTop + window.innerHeight;
+
+    return this.originY <= viewBottom;
+  }
+
+  scrolledOut() {
+    const viewTop = window.pageYOffset;
+
+    return this.originY <= viewTop;
+  }
+
+  toggleClass() {
+    if (this.hasClass()) {
+      this.removeClass();
+    } else {
+      this.addClass();
+    }
+  }
+}
+
+const domAnimationCache = [];
+
+class DomAnimManager {
+  static init() {
+    if (document.readyState in ['interactive', 'complete']) {
+      this.start();
+    } else {
+      document.addEventListener('DOMContentLoaded', () => this.start());
+    }
+  }
+
+  static resetOffsets() {
+    domAnimationCache.forEach(element => requestAnimationFrame(() => element.resetOffsetY()));
+  }
+
+  static scrollCallback() {
+    if (this.scrolled) {
+      this.scrolled = false;
+      domAnimationCache.forEach(element => requestAnimationFrame(() => element.animate()));
+    }
+  }
+
+  static scrollHandler() {
+    this.scrolled = true;
+  }
+
+  static start() {
+    const elements = document.querySelectorAll('[data-anim-name]');
+    elements.forEach(element => domAnimationCache.push(new DomAnimationElement(element)));
+
+    window.addEventListener('scroll', () => this.scrollHandler());
+    window.addEventListener('resize', () => {
+      this.scrollHandler();
+      this.resetOffsets();
+    });
+
+    this.scrolled = true;
+    this.interval = setInterval(() => {
+      this.scrollCallback();
+    }, 50);
+  }
+}
+
+export default DomAnimManager;
---
+++ src/js/form.js
@@ -0,0 +1,76 @@
+export const formClassName = '.contact__form';
+const feedbackMsgClassName = '.contact__form-feedback';
+
+const feedbackMsgs = {
+  fail: '😩 Sorry, that didn’t work. Try again?',
+  loading: null,
+  success: '😃 Message Sent! I’ll get back to you soon!',
+};
+
+const formStateClassNames = {
+  fail: 'fail',
+  loading: 'loading',
+  success: 'success',
+};
+
+const readyStates = {
+  loadingRange: [0, 3],
+  successRange: [200, 299],
+};
+
+const resetForm = function resetForm(form) {
+  const feedbackMsgEl = form.querySelector(feedbackMsgClassName);
+  const classNamesToRemove = Object.keys(formStateClassNames).map(key => formStateClassNames[key]);
+
+  form.classList.remove(...classNamesToRemove);
+  feedbackMsgEl.innerHTML = null;
+};
+
+const readyStateInRange = function readyStateInRange(state, range) {
+  return state >= range[0] && state <= range[1];
+};
+
+const showFormFeedback = function showFormFeedback(form, message, className) {
+  const feedbackMsgEl = form.querySelector(feedbackMsgClassName);
+  const classNamesToRemove = Object.keys(formStateClassNames).map(key => formStateClassNames[key])
+    .filter(formStateClassName => formStateClassName !== className);
+
+  form.classList.remove(...classNamesToRemove);
+  form.classList.add(className);
+  feedbackMsgEl.innerHTML = message;
+};
+
+const onStateChange = function onStateChange(request, form) {
+  if (readyStateInRange(request.readyState, readyStates.loadingRange)) {
+    showFormFeedback(form, feedbackMsgs.loading, formStateClassNames.loading);
+  } else if (readyStateInRange(request.status, readyStates.successRange)) {
+    showFormFeedback(form, feedbackMsgs.success, formStateClassNames.success);
+    setTimeout(() => resetForm(form), 3000);
+  } else {
+    showFormFeedback(form, feedbackMsgs.fail, formStateClassNames.fail);
+  }
+};
+
+const onFormSubmit = function onFormSubmit(event) {
+  event.preventDefault();
+  event.stopPropagation();
+
+  const form = event.currentTarget;
+  resetForm(form);
+
+  if (form.checkValidity()) {
+    const request = new XMLHttpRequest();
+    const formData = new FormData(event.target);
+    request.onreadystatechange = () => { onStateChange(request, event.target); };
+
+    request.open('POST', 'https://formspree.io/njustin@gmail.com', true);
+    request.setRequestHeader('accept', 'application/json');
+    request.send(formData);
+  } else {
+    showFormFeedback(form, feedbackMsgs.fail, formStateClassNames.fail);
+  }
+
+  return false;
+};
+
+export default onFormSubmit;
