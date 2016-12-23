5885fd74e8cd80bfbbe9f43c06c9509b3393c767
Justin <njustin@gmail.com>
2016-12-22 20:59:09 -0800

    Add initial template, themes, js.

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import DomAnimManager from './js/dom_animator';
+import Scene from './js/scene';
+import onFormSubmit, { formClassName } from './js/form';
+
+import './css/app.scss';
+import './css/inline.scss';
+
+(function header() {
+  const canvas = document.getElementById('canvas');
+  const form = document.querySelector(formClassName);
+  const scene = new Scene(canvas, window.devicePixelRatio, 60, Date.now());
+
+  DomAnimManager.init();
+
+  form.addEventListener('submit', event => onFormSubmit(event));
+  window.addEventListener('load', () => scene.load());
+  window.addEventListener('resize', () => scene.updateScale());
+  window.addEventListener('resize', () => scene.updateScale());
+}());
