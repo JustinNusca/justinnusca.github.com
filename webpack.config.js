af71c8aa6d3dea8f4dfbe376d1f80f2d1fa9733f
Justin <njustin@gmail.com>
2017-12-06 21:09:45 -0800

    Update deps, webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -6,95 +6,77 @@ const path = require('path');
 const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 const webpack = require('webpack');
 
-const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+const IS_PROD = process.env.NODE_ENV === 'production';
 
 const PATHS = {
   build: path.resolve(__dirname, 'build'),
   css: path.resolve(__dirname, 'src', 'css', 'app.scss'),
   favIcon: path.resolve(__dirname, 'src', 'favicon.png'),
-  inlineCss: path.resolve(__dirname, 'src', 'css', 'inline.scss'),
   src: path.resolve(__dirname, 'src'),
 };
 
 const cssLoaders = [{
   loader: 'css-loader',
-  query: { minimize: true, sourceMap: !IS_PRODUCTION },
+  query: { minimize: true, sourceMap: !IS_PROD },
 }, {
   loader: 'postcss-loader',
+  options: {
+    ident: 'postcss',
+    plugins: () => [autoprefixer({ browsers: ['last 3 versions', '> 1%'] })],
+    sourceMap: !IS_PROD,
+  },
 }, {
   loader: 'sass-loader',
-  query: { sourceMap: !IS_PRODUCTION, sourceMapContents: !IS_PRODUCTION },
+  query: { sourceMap: !IS_PROD, sourceMapContents: !IS_PROD },
 }];
 
 const plugins = [
-  new CleanWebpackPlugin([PATHS.build], { root: __dirname }),
+  new CleanWebpackPlugin([PATHS.build]),
   new HtmlWebpackPlugin({
     favicon: PATHS.favIcon,
-    hash: false,
-    inject: 'body',
     template: path.resolve(PATHS.src, 'index.html'),
   }),
-  new ExtractTextPlugin('inline.css'),
   new StyleExtHtmlWebpackPlugin(),
-  new webpack.LoaderOptionsPlugin({ options: {
-    postcss: [autoprefixer({ browsers: ['last 3 versions', '> 1%'] })],
-    context: path.resolve(__dirname),
-  } }),
+  new ExtractTextPlugin('app.css'),
 ];
 
-if (!IS_PRODUCTION) {
+if (!IS_PROD) {
   plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: { warnings: false },
     output: { comments: false },
-    sourceMap: true,
+    sourceMap: !IS_PROD,
   }));
 }
 
 module.exports = {
-  devtool: IS_PRODUCTION ? 'nosources-source-map' : 'eval-source-map',
-  entry: {
-    app: path.resolve(PATHS.src, 'app.js'),
+  devtool: IS_PROD ? 'nosources-source-map' : 'eval-source-map',
+  entry: { app: path.resolve(PATHS.src, 'app.js') },
+  module: {
+    rules: [{
+      include: PATHS.css,
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: cssLoaders,
+      }),
+    }, {
+      test: /\.(gif|jpeg|jpg|png|svg)$/,
+      use: 'url-loader',
+    }, {
+      test: /\.html$/,
+      use: 'html-loader',
+    }, {
+      include: PATHS.src,
+      test: /\.js$/,
+      use: 'babel-loader',
+    }, {
+      test: /(CNAME)$/,
+      loader: 'static-loader',
+    }],
   },
   output: {
     filename: '[name].[hash:8].js',
     path: PATHS.build,
-  },
-  module: {
-    loaders: [
-      {
-        test: /(CNAME)$/,
-        loader: 'static-loader',
-        query: { output: 'CNAME' },
-      }, {
-        test: /\.(gif|jpeg|jpg|png|svg)$/,
-        loader: 'url-loader',
-        query: { limit: 10000, name: 'assets/[name].[hash:8].[ext]' },
-      }, {
-        test: /\.html$/,
-        loader: 'html-loader',
-        query: { interpolate: true, minimize: true },
-      }, {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          presets: [['es2015', { modules: false }]],
-        },
-      }, {
-        test: /\.scss$/,
-        exclude: PATHS.inlineCss,
-        include: PATHS.css,
-        loader: [{ loader: 'style-loader' }].concat(cssLoaders),
-      }, {
-        test: /\.scss$/,
-        exclude: PATHS.css,
-        include: PATHS.inlineCss,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader',
-          loader: cssLoaders,
-        }),
-      },
-    ],
   },
   plugins,
 };
